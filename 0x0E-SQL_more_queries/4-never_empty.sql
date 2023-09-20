08972de1542ddf881c9d320438ebb2b3862fc90b
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-09-20 13:25:41 -0700

    updated

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,7 @@
--- Creates the table force_name.
-CREATE TABLE IF NOT EXISTS `force_name` (
-    `id`   INT,
-    `name` VARCHAR(256) NOT NULL
+-- Create a table
+-- Execute: cat 4-never_empty.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+CREATE TABLE IF NOT EXISTS `id_not_null`
+(
+	id INT DEFAULT 1,
+	name VARCHAR(256)
 );
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,7 @@
--- Creates the table id_not_null
-CREATE TABLE IF NOT EXISTS `id_not_null` (
-    `id`   INT          DEFAULT 1,
-    `name` VARCHAR(256)
+-- Create a table with UNIQUE Column
+-- Execute: cat 5-unique_id.sql | mysql -hlocalhost -uroot -p hbtn_0d_2 
+CREATE TABLE IF NOT EXISTS `unique_id`
+(
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
 );
