51ebdd08c05dfb4b1f1c2dd8cfddabc97b60cd96
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-11-02 12:43:30 -0700

    updated

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script to send custom headers to servers
-
+# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
