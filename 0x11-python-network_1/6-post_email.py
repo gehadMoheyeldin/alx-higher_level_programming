a89c5312f9a82124c4b9fd67ba8eb6c6c554cbb9
gehadMoheyeldin <gehad.mohey47@gmail.com>
2023-11-03 05:02:50 -0700

    updated

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,13 @@
-i#!/usr/bin/python3
-"""
-takes in a URL and an email address, sends a POST request to the passed URL
-with the email as a parameter, and finally displays the body of the response
-"""
-if __name__ == '__main__':
-    import requests
-    from sys import argv
-    payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
-    print(r.text)
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email as a parameter, and
+finally displays the body of the response."""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {"email": sys.argv[2]}
+
+    req = requests.post(url, data=email)
+    print(req.text)
